98d51e56447f8a7243871e9430c1600cfa875b52
Krecikkko <qubaqq2015@gmail.com>
2025-03-09 22:18:49 +0100

    Sample program with test

---
+++ Task0/Task0/Calculator.cs
@@ -0,0 +1,25 @@
+﻿namespace Task0
+{
+    public class Calculator
+    {
+        public double add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double substract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double divide(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ Task0/Task0Test/CalculatorTest.cs
@@ -0,0 +1,36 @@
+﻿using Task0;
+
+namespace Task0Test
+{
+    [TestClass]
+    public sealed class CalculatorTest
+    {
+        [TestMethod]
+        public void add_addTwoNumbers_correctResult()
+        {
+            var calculator = new Task0.Calculator();
+            Assert.AreEqual(3, calculator.add(1, 2));
+        }
+
+        [TestMethod]
+        public void substract_substractTwoNumbers_correctResult()
+        {
+            var calculator = new Task0.Calculator();
+            Assert.AreEqual(1, calculator.substract(2, 1));
+        }
+
+        [TestMethod]
+        public void multiply_multiplyTwoNumbers_correctResult()
+        {
+            var calculator = new Task0.Calculator();
+            Assert.AreEqual(6, calculator.multiply(2, 3));
+        }
+
+        [TestMethod]
+        public void divide_divideTwoNumbers_correctResult()
+        {
+            var calculator = new Task0.Calculator();
+            Assert.AreEqual(2, calculator.divide(6, 3));
+        }
+    }
+}
