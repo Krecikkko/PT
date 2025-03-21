dea53b177bc445d49258a229568a7eb0ac3fad47
Krecikkko <qubaqq2015@gmail.com>
2025-03-21 09:10:05 +0100

    Synatx changes

--- Task0/Task0Test/CalculatorTest.cs
+++ Task0/Task0Test/CalculatorTest.cs
@@ -8,35 +8,35 @@ namespace Task0Test
         [TestMethod]
         public void add_addTwoNumbers_correctResult()
         {
-            var calculator = new Task0.Calculator();
+            var calculator = new Calculator();
             Assert.AreEqual(3, calculator.add(1, 2));
         }
 
         [TestMethod]
         public void substract_substractTwoNumbers_correctResult()
         {
-            var calculator = new Task0.Calculator();
+            var calculator = new Calculator();
             Assert.AreEqual(1, calculator.substract(2, 1));
         }
 
         [TestMethod]
         public void multiply_multiplyTwoNumbers_correctResult()
         {
-            var calculator = new Task0.Calculator();
+            var calculator = new Calculator();
             Assert.AreEqual(6, calculator.multiply(2, 3));
         }
 
         [TestMethod]
         public void divide_divideTwoNumbers_correctResult()
         {
-            var calculator = new Task0.Calculator();
+            var calculator = new Calculator();
             Assert.AreEqual(2, calculator.divide(6, 3));
         }
 
         [TestMethod]
         public void modulo_divideTwoNumbers_correctResult()
         {
-            var calculator = new Task0.Calculator();
+            var calculator = new Calculator();
             Assert.AreEqual(2, calculator.modulo(5, 3));
         }
     }
