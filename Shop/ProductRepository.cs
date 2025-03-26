4721ebc18a27ff8d0d6bec0bd31286a5454384ae
Krecikkko <qubaqq2015@gmail.com>
2025-03-26 22:57:23 +0100

    Shop

---
+++ Shop/ProductRepository.cs
@@ -0,0 +1,24 @@
+﻿namespace Shop.Data
+{
+    public class Product
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public decimal price { get; set; }
+    }
+
+    public interface IProductRepository
+    {
+        Product getById(int id);
+        void add(Product product);
+    }
+
+    public class InMemoryProductRepository : IProductRepository
+    {
+        private readonly Dictionary<int, Product> _products = new();
+
+        public Product getById(int id) => _products.TryGetValue(id, out var product) ? product : null;
+
+        public void add(Product product) => _products[product.id] = product;
+    }
+}
