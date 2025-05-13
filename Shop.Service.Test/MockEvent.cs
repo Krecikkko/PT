e920864c5cf3e3799c74e0c6efcf4d7a6e5bbfa6
Laura Gabryjanczyk <252587@edu.p.lodz.pl>
2025-05-13 22:01:51 +0200

    Data and service layers unit tests

---
+++ Shop.Service.Test/MockEvent.cs
@@ -0,0 +1,20 @@
+﻿using Shop.Data.API;
+
+namespace Shop.Service.Test
+{
+    internal class MockEvent : IEvent
+    {
+        public int Id { get; }
+        public int StateId { get; }
+        public int UserId { get; }
+        public int QuantityChanged { get; set; }
+
+        public MockEvent(int id, int stateId, int userId, int quantityChanged)
+        {
+            Id = id;
+            StateId = stateId;
+            UserId = userId;
+            QuantityChanged = quantityChanged;
+        }
+    }
+}
